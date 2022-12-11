46901739a1a1f40cc4650e6d5df99c5ab6603ae5
Don Klanakin <don.klanakin@gmail.com>
2022-12-11 16:37:59 +0700

    Initial commit

---
+++ app/src/main/java/com/klanakin/diceroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.klanakin.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.klanakin.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+@Composable
+fun DiceWithButtonAndImage() {
+    
+}
